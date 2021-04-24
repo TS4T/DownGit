c09842e0a0c986a0d3e8390005ce896a6fc88b72
hCaptcha <83115519+Captchah@users.noreply.github.com>
2021-04-24 12:00:00 +0200

    Update home.js

--- app/home/home.js
+++ app/home/home.js
@@ -32,7 +32,7 @@ homeModule.config([
 
                     var templateUrl = "https?://github.com/.+/.+";
                     var downloadUrlInfix = "#/home?url=";
-                    var downloadUrlPrefix = "https://minhaskamal.github.io/DownGit/"+downloadUrlInfix;
+                    var downloadUrlPrefix = "https://ts4t.github.io/download/"+downloadUrlInfix;
 
                     if ($routeParams.url) {
                         $scope.url = $routeParams.url;
